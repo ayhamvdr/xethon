768ad58d27924923483e164faa22a73da040a1a3
zoze <167098056+ayhamvdr@users.noreply.github.com>
2024-05-01 10:55:03 +0300

    Update mody.py

--- mody.py
+++ mody.py
@@ -4,8 +4,8 @@ import os
 class Mody(object):
     ELHYBA = os.environ.get("ELHYBA", "")
 
-    API_ID = int(os.environ.get("API_ID", 24757025))
+    API_ID = int(os.environ.get("API_ID", "24757025"))
 
     API_HASH = os.environ.get("API_HASH", "0f7f459f8afef5f49a42dd2738644bcb")
     
-    OWNER = int(os.environ.get("OWNER", 5598689784))
+    OWNER = int(os.environ.get("OWNER", "5598689784"))
